22ec37ffa3338443dd994c5fba893b5e0c336a88
rivash728 <rivash38@protonmail.com>
2023-01-17 15:23:40 -0500

    Biography Exercise Complete

---
+++ Section 02/Biography.java
@@ -0,0 +1,20 @@
+public class Biography {
+    
+    public static void main(String[] args) {
+        
+        String name = "Henry";
+        int age = 29;
+        String country = "USA";
+        String sport = "Taekwando";
+        int hours = 4;
+        String videoGame = "Fallout New Vegas";
+        String subject = "History";
+        char grade = 'B';
+
+        System.out.println("My name is " + name + ". I'm " + age + "years old, and I'm from " + country);
+        System.out.println("My favorite sport is " + sport + ". I play for " + hours + " hours(s) a day");
+        System.out.println("When I'm tired, I like to play " + videoGame + ".");
+        System.out.println("In school, my favorite subject was" + subject + ". I scored a " + grade);
+
+    }
+}
