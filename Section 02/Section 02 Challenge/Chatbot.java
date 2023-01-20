e384e44fa5c327c7c21f9b9c2c5db651892291a3
rivash728 <rivash38@protonmail.com>
2023-01-20 16:22:01 -0500

    Section 02 Challenge Complete
    initial commit of completed challenge project

--- Section 02/Section 02 Challenge/Chatbot.java
+++ Section 02/Section 02 Challenge/Chatbot.java
@@ -13,5 +13,18 @@ public class Chatbot{
         String place = scan.nextLine();
 
         System.out.println("\n I hear it's beautiful at " + place + "I'm from a place called Oracle");
+        System.out.println("How old are you?");
+
+        int age = scan.nextInt();
+
+        System.out.println("\nSo you are " + age + " cool! I am " + (age * 8) + " years old.");
+        System.out.println("This means I'm 8 times older than you.");
+
+        scan.nextLine();
+
+        System.out.println("\nEnough about me. What's your favorite language? (just don't say Python");
+        String language = scan.nextLine();
+
+        System.out.println( language + ", that's great! Nice chatting with you!");
     }
 }
