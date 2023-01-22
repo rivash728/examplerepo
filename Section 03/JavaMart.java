aa189854bfc28e0a577a3c1fad2d18a3bca2f352
rivash728 <rivash38@protonmail.com>
2023-01-21 21:53:54 -0500

    JavaMart Exercise Complete
    initial commit to javamart exercise from Section 3

---
+++ Section 03/JavaMart.java
@@ -0,0 +1,26 @@
+public class JavaMart {
+    
+    public static void main(String[] args) {
+        
+        double wallet = 100;
+        double car = 100;
+
+        System.out.println("Can I get this car?");
+        if(wallet >= car){
+            System.out.println("Sure");
+        } else {
+            System.out.println("Sorry! I only have " + wallet + " left");
+        }
+
+        wallet = 94.01;
+        double price = 129.99;
+
+        System.out.println("Can I get these nike shoes?");
+        if(wallet > price){
+            System.out.println("Sure!");
+        } else  {
+            System.out.println("Sorry! I only have " + wallet + " left");
+        }
+
+    }
+}
